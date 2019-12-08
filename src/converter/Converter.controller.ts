24311b00755c9114a99815eb29a0d4f5b5ef9a3e
Andriy <andsei91@gmail.com>
2019-12-08 02:48:37 +0100

    * TEST

--- src/converter/Converter.controller.ts
+++ src/converter/Converter.controller.ts
@@ -9,6 +9,8 @@ import { Logger } from "@overnightjs/logger";
 import { FileManager } from "../filemanager/FileManager";
 import { basename } from "path";
 import { lookup } from 'mime-types';
+import { WritableStream, ReadableStream } from "memory-streams";
+import { PassThrough } from "stream";
 
 @Controller("")
 export class ConverterController {
@@ -42,6 +44,9 @@ export class ConverterController {
      *            name: from
      *            type: string
      *            required: true
+     *          - in: formaData
+     *            name: metadata
+     *            type: string
      *      responses:
      *          200:
      *              description: ok
@@ -55,12 +60,17 @@ export class ConverterController {
     @Post("convert")
     @Catch
     public async convert(req: Request, res: Response): Promise<void> {
-        const {format, id, from} = req.body;
+        const {format, id, from, metadata} = req.body;
         let {tags} = req.body;
         if (!tags) {
             tags = [];
         } else {
             tags = tags.split(",");
+        }
+
+        let parsedMetadata = {};
+        if (typeof metadata === "string") {
+            parsedMetadata = JSON.parse(metadata);
         }
         
         if (!req.files || !Object.keys(req.files).length) {
@@ -73,16 +83,20 @@ export class ConverterController {
             throw new HttpError("Format/From not valid", BAD_REQUEST);
         }
 
-        const file = req.files.file as UploadedFile;
-        const {writeStream, done} = Converter.convert({
-            file: Converter.bufferToStream(file.data),
-            format
-        });
-        done.catch((error) => Logger.Err(error, true));
         res.status(OK).json({ok: true});
         try {
+            const file = req.files.file as UploadedFile;
+            const writeStream = new PassThrough();
+            Converter.convert({
+                file: Converter.bufferToStream(file.data),
+                format,
+                metadata: parsedMetadata,
+                writeTo: writeStream as any
+            }).catch((error) => Logger.Err(error, true));
+            const readStream = new PassThrough();
+            writeStream.pipe(readStream);
             await FileManager.upload({
-                file: writeStream as any,
+                file: readStream as any,
                 filename: `${basename(file.name, `.${from}`)}.${format}`,
                 id: `${id}:${format}`, tags
             });
@@ -113,6 +127,9 @@ export class ConverterController {
      *            name: from
      *            type: string
      *            required: true
+     *          - in: formData
+     *            name: metadata
+     *            type: string
      *      responses:
      *          200:
      *              description: ok
@@ -129,7 +146,12 @@ export class ConverterController {
     @Post("convert-and-download")
     @Catch
     public async convertAndDownload(req: Request, res: Response): Promise<void> {
-        const {format, from} = req.body;
+        const {format, from, metadata} = req.body;
+
+        let parsedMetadata = {};
+        if (typeof metadata === "string") {
+            parsedMetadata = JSON.parse(metadata);
+        }
         
         if (!req.files || !Object.keys(req.files).length) {
             throw new HttpError("No file was sent", BAD_REQUEST);
@@ -139,17 +161,85 @@ export class ConverterController {
         }
 
         const file = req.files.file as UploadedFile;
-        const {writeStream, done} = Converter.convert({
-            file: Converter.bufferToStream(file.data),
-            format
-        });
-
         const filename = `${basename(file.name, `.${from}`)}.${format}`;
         res.setHeader("Content-disposition", "attachment; filename=" + filename);
         res.setHeader("x-suggested-filename", filename);
         res.setHeader("content-type", lookup(filename) || "application/octet-stream");
-        writeStream.pipe(res);
-        await done;
+
+        await Converter.convert({
+            file: Converter.bufferToStream(file.data),
+            format,
+            metadata: parsedMetadata,
+            writeTo: res as any
+        });
+
+        res.end(OK);
+    }
+
+    /**
+     * @swagger
+     * /convert-mp4-mp3-and-download:
+     *  post:
+     *      tags:
+     *          - convert
+     *      consumes:
+     *          - multipart/form-data
+     *      parameters:
+     *          - in: formData
+     *            name: file
+     *            type: file
+     *            description: File to upload
+     *            required: true
+     *          - in: formData
+     *            name: image
+     *            type: file
+     *            description: Cover as file
+     *          - in: formData
+     *            name: imageUrl
+     *            type: string
+     *          - in: formData
+     *            name: metadata
+     *            type: string
+     *      responses:
+     *          200:
+     *              description: ok
+     *              content:
+     *                  video/mp3:
+     *                      schema:
+     *                          type: string
+     *                          format: binary
+     */
+    @Post("convert-mp4-mp3-and-download")
+    @Catch
+    public async convertMp4Mp3AndDownload(req: Request, res: Response): Promise<void> {
+        const {metadata, imageUrl} = req.body;
+
+        let parsedMetadata = {};
+        if (typeof metadata === "string") {
+            parsedMetadata = JSON.parse(metadata);
+        }
+        
+        if (!req.files || !Object.keys(req.files).length) {
+            throw new HttpError("No file was sent", BAD_REQUEST);
+        }
+
+        const {file, image}: {file: UploadedFile, image: UploadedFile} = req.files as any;
+        const rawFilename = basename(file.name, ".mp4");
+        const fileStream = Converter.bufferToStream(file.data);
+        const imageStream = Converter.bufferToStream(image.data);
+        const filename = `${rawFilename}.mp3`;
+        res.setHeader("Content-disposition", "attachment; filename=" + filename);
+        res.setHeader("x-suggested-filename", filename);
+        res.setHeader("content-type", lookup(filename) || "application/octet-stream");
+        
+        await Converter.convertMp4ToMp3({
+            file: fileStream as any,
+            filename: rawFilename,
+            image: imageStream as any,
+            metadata,
+            writeTo: res as any
+        });
+
         res.end(OK);
     }
 
--- src/converter/Converter.ts
+++ src/converter/Converter.ts
@@ -1,27 +1,110 @@
 import ffmpeg from "fluent-ffmpeg";
 import { Readable, PassThrough } from "stream";
-import { WriteStream } from "fs";
+import { createReadStream, promises, WriteStream, ReadStream } from "fs";
+import Axios from "axios";
+import { join } from "path";
+import { tmpdir } from "os";
+import { WritableStream } from "memory-streams";
 
 export class Converter {
 
     public static convert({
-        file, format
-    }: IConvertArgs): IConvertResponse {
-        const write = new PassThrough();
-        const promise = new Promise<void>((resolve, reject) => {
-            ffmpeg({
-                source: file
-            })
-            .toFormat(format)
-            // .outputOptions("-metadata", `artist=seirius`)
-            .writeToStream(write)
+        file, format, metadata, writeTo
+    }: IConvertArgs): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            const ff = ffmpeg().input(file);
+            if (metadata) {
+                Object.entries(metadata)
+                .forEach(([key, value]) => ff.outputOptions("-metadata", `${key}=${value}`));
+            }
+            ff.toFormat(format)
+            .pipe(writeTo)
+            .on("error", reject)
+            .on("finish", resolve);
+        });
+    }
+
+    public static async convertMp4ToMp3({
+        file, 
+        image: {
+            image,
+            url
+        }, metadata, filename, writeTo
+    }: IConvertMp4ToMp3Args): Promise<void> {
+        const auxWrite = new PassThrough();
+        console.log(0);
+        await Converter.convert({
+            file: file,
+            format: "mp3",
+            metadata,
+            writeTo: auxWrite as any
+        });
+        console.log(0.5);
+        if (!image && !url) {
+            auxWrite.pipe(writeTo);
+            return;
+        }
+        let imageStream: any;
+        if (image) {
+            imageStream = image;
+        } else {
+            imageStream = new PassThrough() as any;
+            (await Axios({
+                url: url,
+                method: "GET",
+                responseType: "stream"
+            })).data.pipe(imageStream);
+        }
+        console.log(1);
+        
+        const tempPathFilename = join(tmpdir(), filename + ".mp3");
+        const tempPathImage = join(tmpdir(), filename + "image");
+        
+        await promises.writeFile(tempPathImage, imageStream);
+        console.log(2);
+        
+        const auxRead = new PassThrough();
+        auxWrite.pipe(auxRead);
+        console.log(3);
+        await Converter.addImageToMp3({
+            audio: auxRead,
+            imagePath: tempPathImage,
+            savePath: tempPathFilename
+        });
+        console.log(4);
+
+        await Converter.pipeTempMp3AndRemove({
+            tempFilename: tempPathFilename,
+            writeTo
+        });
+    }
+
+    public static addImageToMp3({audio, imagePath, savePath}: IAddImageToMp3): Promise<void> {
+        return new Promise((resolve, reject) => {
+            ffmpeg(audio)
+            .addInput(imagePath)
+            .outputOptions([
+                "-map", "0:0", "-map", "1:0",
+                "-c", "copy", "-id3v2_version", "3"
+            ])
+            .save(savePath)
             .on("finish", resolve)
             .on("error", reject);
         });
-        return {
-            writeStream: write as any,
-            done: promise
-        }
+    }
+
+    private static pipeTempMp3AndRemove({tempFilename, writeTo}: {
+        tempFilename: string;
+        writeTo: WriteStream;
+    }): Promise<void> {
+        return new Promise((resolve, reject) => {
+            createReadStream(tempFilename)
+            .pipe(writeTo)
+            .on("finish", async () => {
+                await promises.unlink(tempFilename);
+                resolve(); 
+            }).on("error", reject);
+        });
     }
 
     public static bufferToStream(buffer: Buffer): Readable {
@@ -37,9 +120,23 @@ export class Converter {
 export interface IConvertArgs {
     file: Readable;
     format: string;
+    metadata?: Record<string, any>;
+    writeTo: WriteStream;
 }
 
-export interface IConvertResponse {
-    writeStream: WriteStream;
-    done: Promise<void>;
+export interface IConvertMp4ToMp3Args {
+    file: ReadStream;
+    image: {
+        image: Buffer;
+        url: string;
+    };
+    metadata?: Record<string, any>;
+    filename: string;
+    writeTo: WriteStream;
+}
+
+export interface IAddImageToMp3 {
+    audio: any;
+    imagePath: string;
+    savePath: string;
 }
