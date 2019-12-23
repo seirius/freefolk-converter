58af53a6d0f6638b141a6d2cb853988a6b09ad99
Andriy <andsei91@gmail.com>
2019-12-23 22:47:19 +0100

    fix: mqtt

--- src/converter/converter.module.ts
+++ src/converter/converter.module.ts
@@ -2,10 +2,9 @@ import { Module } from "@nestjs/common";
 import { ConverterService } from "./converter.service";
 import { ConverterController } from "./converter.controller";
 import { FileManagerModule } from "./../filemanager/filemanager.module";
-import { MqttModule } from "nest-mqtt-client";
 
 @Module({
-    imports: [FileManagerModule, MqttModule],
+    imports: [FileManagerModule],
     providers: [ConverterService],
     controllers: [ConverterController]
 })
